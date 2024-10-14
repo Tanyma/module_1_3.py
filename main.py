59bc397226baee3ce5e763de277973069d45c04d
Tanyma <165762540+Tanyma@users.noreply.github.com>
2024-10-14 16:23:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "Валя"
+print (f'Name: {name}', type(name))
+age = 19
+print (f'Age: {age}', type(age))
+new_age = age + 1
+print (new_age)
+Student = age < new_age
+print (f'Is Student: {Student}', type(Student))
