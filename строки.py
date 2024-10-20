2318e2f04517076c072f6bbc69f7952032833555
Tanyma <165762540+Tanyma@users.noreply.github.com>
2024-10-20 13:31:42 +0300

    Add files via upload

---
+++ строки.py
@@ -0,0 +1,12 @@
+example = "Chocolate"
+
+print (f'Первый символ строки: "{example [0]}"')
+
+print (f"Последний символ строки: '{example [-1]}'")
+
+exe = len(example) // 2
+print (example [exe:])
+
+print (example [::-1])
+
+print (example [1::2])
